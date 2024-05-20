e3b9eddd941051734be2a1e43c45cd4e7b4b8799
Sofía Alvarado <alvaradosofiaH@outlook.com>
2024-05-19 19:31:39 -0600

    Adding routes and handling errors

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const productRoutes = require('./routes/products');
+const orderRoutes = require('./routes/orders');
 
+app.use(morgan('dev'));
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+// Handling errors
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+    const error = new Error('Not found');
+    error.status = 404
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
