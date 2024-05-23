4d5ce5700dcb0c183266beb9bc644daad1bb3707
Sofía Alvarado <alvaradosofiaH@outlook.com>
2024-05-22 23:36:17 -0600

    Refactoring Product Model and Controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,41 +1,53 @@
-const express = require ('express');
-const router = express.Router();
-const db = require('../config/db')
-const service = require('../services/products')
+const Product = require('../models/product');
 
-router.get('/', async (req, res) => {
-    const products = await service.getAllProducts()
-    res.send(products)
-})
+const getAllProducts = async (req, res) => {
+  try {
+    const product = await Product.getAllProducts();
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-router.get('/:id', async (req, res) => {
-    const product = await service.getProductById(req.params.id);
+const getProductById = async (req, res) => {
+  try {
+		const product = await Product.getProductById(req.params.id);
     console.log(product)
     if (product.length == 0) {
-        // res.status(404).json('no record with given id: ' + req.params.id);
         res.status(404).json({
             error: 'No product with id ' + req.params.id
         });
     } else {
         res.send(product);
     }
-})
+  } catch {
+		res.status(500).json({ message: error.message })
+  }
+};
 
-router.delete('/:id', async (req, res) => {
-    const affectedProduct= await service.deleteProduct(req.params.id);
+const createEditProduct = async (req, res) => {
+	await Product.createEditProduct(req.body);
+	res.status(201).send('Created succesfully');
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+		const affectedProduct= await Product.deleteProduct(req.params.id);
     if (affectedProduct == 0) {
-        // res.status(404).json('No record with the given id: ' + req.params.id)
         res.status(404).json({
             error: 'No product with id ' + req.params.id
         });
     } else {
         res.send('Deleted successfully');
     }
-})
+	} catch {
+		res.status(500).json({ message: error.message })
+	}
+}
 
-router.post('/', async (req, res) => {
-    await service.addOrEditProduct(req.body);
-    res.status(201).send('Created succesfully');
-})
-
-module.exports = router;
+module.exports = {
+    getAllProducts,
+    getProductById,
+    createEditProduct,
+    deleteProduct
+};
---
+++ models/product.js
@@ -0,0 +1,26 @@
+const db = require('../config/db');
+
+const Product = {
+  getAllProducts: async () => {
+    const [[products]] = await db.query('CALL sp_show_products()')
+    return { products: products} ;
+  },
+
+	getProductById: async (id) => {
+		const [[product]] = await db.query('CALL sp_show_product_by_id(?)', [id]);
+    return product;
+	},
+
+	createEditProduct: async (obj, id = 0) => {
+		const [{affectedRows}] = await db.query('CALL sp_add_edit_product(?,?,?,?,?,?,?,?)', 
+    [id, obj.product_name, obj.description, obj.price, obj.size, obj.product_img_path, obj.id_brand, obj.id_category])
+    return affectedRows;
+	},
+
+	deleteProduct: async (id) => {
+		const [{affectedRows}] = await db.query('DELETE FROM product WHERE id = ?', [id])
+    return affectedRows;
+	}
+}
+
+module.exports = Product;
