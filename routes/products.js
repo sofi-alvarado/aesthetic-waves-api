4f081549468dac16babe30364d477a1855af490a
Sofía Alvarado <alvaradosofiaH@outlook.com>
2024-05-22 23:35:24 -0600

    Refactoring routes

--- routes/products.js
+++ routes/products.js
@@ -1,55 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET request to /products'
-    })
+const productController = require('../controllers/products')
 
-    const sql = "SELECT * FROM product";
-    db.query(sql, (err, data) => {
-        if (err) return res.json("error");
-        return res.json({products: data});
-    })
-})
-
-
-router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    };
-
-    res.status(201).json({
-        messagge: 'Handling Post request to /products',
-        createdProduct: product
-    })
-})
-
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You discovered a special ID'
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        })
-    }
-})
-
-router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated Product'
-    })
-})
-
-router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted Product'
-    })
-})
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+router.post('/', productController.createEditProduct);
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
